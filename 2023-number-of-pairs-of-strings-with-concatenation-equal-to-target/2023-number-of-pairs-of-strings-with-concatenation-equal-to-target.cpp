9fbd6ccab2bd869857d043aabea0b5af4f83520f
Gayathri <gayathrir1104@gmail.com>
2023-02-19 13:13:09 -0500

    Time: 14 ms (81.77%), Space: 11.2 MB (78.49%) - LeetHub

--- 2023-number-of-pairs-of-strings-with-concatenation-equal-to-target/2023-number-of-pairs-of-strings-with-concatenation-equal-to-target.cpp
+++ 2023-number-of-pairs-of-strings-with-concatenation-equal-to-target/2023-number-of-pairs-of-strings-with-concatenation-equal-to-target.cpp
@@ -13,20 +13,15 @@ public:
         int c=0;
         for(int i=0;i<l;i++){
             int sl = nums[i].size();
-            // string s = ;
-            // string s1 = ;
-            //cout  << s << " " << s1 << "r" << endl;
             if(nums[i]==target.substr(0,sl)){
                 if(mp.find(target.substr(sl,t))!=mp.end()){
                     if(target.substr(sl,t)==target.substr(0,sl)){
                         if(mp[target.substr(sl,t)]-1>0){
                             c = c + mp[target.substr(sl,t)]-1;
-                            //cout << c << "h" << endl;
                         }
                     }
                     else{
                         c = c+mp[target.substr(sl,t)];
-                        //cout << c << "t" << endl;
                     }
                 }
             }
