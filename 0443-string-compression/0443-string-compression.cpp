f7c301e6d1111292ce8c72222a3c5c5f3061fae5
Gayathri <gayathrir1104@gmail.com>
2023-03-02 19:05:14 -0500

    Time: 7 ms (42.97%), Space: 8.9 MB (82.85%) - LeetHub

--- 0443-string-compression/0443-string-compression.cpp
+++ 0443-string-compression/0443-string-compression.cpp
@@ -5,34 +5,53 @@ public:
         if(l==0){
             return 0;
         }
-        char c = chars[0];
+        //char c = chars[0];
         int co = 1;
         string s;
-        for(int i=1;i<l;i++){
-            if(chars[i]==c){
+        int i,k = 0;
+        for(i=1;i<l;i++){
+            if(chars[i]==chars[i-1]){
                 co++;
             }
             else{
-                s.push_back(c);
-                cout << c << " " << "h" << endl;
+                chars[k]=chars[i-1];
+                cout << k << endl;
+                k++;
+                // s.push_back(c);
+                // cout << c << " " << "h" << endl;
                 if(co>1){
                     string ss = to_string(co);
-                    s = s + ss;
+                    for(int j=0;j<ss.size();j++){
+                        chars[k] = ss[j];
+                        cout << k << endl;
+                        k++;
+                    }
+                   // s = s + ss;
                 }
-                c = chars[i];
+                //c = chars[i];
                 co = 1;
             }
         }
-        s.push_back(c);
+        // s.push_back(c);
+        // if(co>1){
+        //     string ss = to_string(co);
+        //     s = s + ss;
+        // }
+        chars[k] = chars[i-1];
+        k++;
         if(co>1){
             string ss = to_string(co);
-            s = s + ss;
+            for(int j=0;j<ss.size();j++){
+                chars[k] = ss[j];
+                cout << k << endl;
+                k++;
+            }
         }
         
-        
-        for(int i=0;i<s.size();i++){
-            chars[i] = s[i];
-        }
-        return s.size();
+        // for(int i=0;i<s.size();i++){
+        //     chars[i] = s[i];
+        // }
+        // return s.size();
+        return k;
     }
 };
