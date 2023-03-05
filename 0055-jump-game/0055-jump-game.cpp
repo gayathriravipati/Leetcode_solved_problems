37d04d228f6d3651b97c92fb69a75a57c9b481ba
Gayathri <gayathrir1104@gmail.com>
2023-03-05 17:19:45 -0500

    Time: 73 ms (55.72%), Space: 48.3 MB (80.59%) - LeetHub

--- 0055-jump-game/0055-jump-game.cpp
+++ 0055-jump-game/0055-jump-game.cpp
@@ -2,28 +2,28 @@ class Solution {
 public:
     bool canJump(vector<int>& nums) {
         int l = nums.size();
-        vector<int>v(l,0);
-        for(int i=l-1;i>=0;i--){
-            if(nums[i]+i>=l-1){
-                v[i] = 1;
-            }
-            else{
-                for(int j = nums[i];j>=1;j--){
-                    if(v[i+j]==1){
-                        v[i] = 1;
-                        break;
-                    }
-                }
-            }
-        }
-        return v[0]==1;
-        
-        // int lp = nums.size()-1;
+        // vector<int>v(l,0);
         // for(int i=l-1;i>=0;i--){
-        //     if(nums[i]+i>=lp){
-        //         lp = i;
+        //     if(nums[i]+i>=l-1){
+        //         v[i] = 1;
+        //     }
+        //     else{
+        //         for(int j = nums[i];j>=1;j--){
+        //             if(v[i+j]==1){
+        //                 v[i] = 1;
+        //                 break;
+        //             }
+        //         }
         //     }
         // }
-        // return lp==0;
+        // return v[0]==1;
+        
+        int lp = nums.size()-1;
+        for(int i=l-1;i>=0;i--){
+            if(nums[i]+i>=lp){
+                lp = i;
+            }
+        }
+        return lp==0;
     }
 };
