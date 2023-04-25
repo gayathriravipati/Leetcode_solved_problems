824cd0d5f105103f2b7167bb3cdb1fbd11466c1f
Gayathri <gayathrir1104@gmail.com>
2023-04-25 10:15:37 -0400

    Time: 80 ms (82.58%), Space: 35.4 MB (92.24%) - LeetHub

---
+++ 2336-smallest-number-in-infinite-set/2336-smallest-number-in-infinite-set.cpp
@@ -0,0 +1,34 @@
+class SmallestInfiniteSet {
+    unordered_set<int> isPresent;
+    priority_queue<int, vector<int>, greater<int>> addedIntegers;
+    int currentInteger;
+
+public:
+    SmallestInfiniteSet() {
+        currentInteger = 1;
+    }
+
+    int popSmallest() {
+        int answer;
+        if (!addedIntegers.empty()) {
+            answer = addedIntegers.top();
+            isPresent.erase(answer);
+            addedIntegers.pop();
+        } 
+       
+        else {
+            answer = currentInteger;
+            currentInteger += 1;
+        }
+        return answer;
+    }
+
+    void addBack(int num) {
+        if (currentInteger <= num || 
+            isPresent.find(num) != isPresent.end()) {
+            return;
+        }
+        addedIntegers.push(num);
+        isPresent.insert(num);
+    }
+};
