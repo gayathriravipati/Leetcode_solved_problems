b675dc91c87b4883fdb69622d3e4e50ec6fc8733
Gayathri <gayathrir1104@gmail.com>
2023-12-27 07:40:35 -0500

    Time: 22 ms (73.64%), Space: 9 MB (29.69%) - LeetHub

--- 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.cpp
+++ 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.cpp
@@ -1,24 +1,3 @@
-// class Solution {
-// public:
-//     const int mod = (int)pow(10, 9) + 7; 
-//     int numRollsToTarget(int n, int k, int target) {
-//         vector<vector<int>> dp(n + 1, vector<int>(target + 1, -1)); 
-//         return recursion(dp, n, k, target); 
-//     }
-
-// private:
-//     int recursion(vector<vector<int>>& dp, int n, int k, int target) {
-//         if (target == 0 && n == 0) return 1;
-//         if (n == 0 || target <= 0) return 0;
-//         if (dp[n][target] != -1) return dp[n][target];
-//         int ways = 0; 
-//         for (int i = 1; i <= k; i++) {
-//             ways = (ways + recursion(dp, n - 1, k, target - i)) % mod;
-//         }
-//         return ways % mod; 
-//     }
-// };
-
 class Solution {
 public:
     const int mod = (int)pow(10, 9) + 7;
