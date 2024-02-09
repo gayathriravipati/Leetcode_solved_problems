e8adb7b0b9c56f57d740215229a5b5a821530b83
Gayathri <gayathrir1104@gmail.com>
2024-02-09 15:44:20 -0500

    Time: 14 ms (83.03%), Space: 11.2 MB (41.61%) - LeetHub

--- 0368-largest-divisible-subset/0368-largest-divisible-subset.cpp
+++ 0368-largest-divisible-subset/0368-largest-divisible-subset.cpp
@@ -4,10 +4,10 @@ public:
         sort(nums.begin(), nums.end());
         vector<int> res;
         int l = nums.size();
-        if (l == 0) return res; // handle edge case of empty input
-        vector<int> dp(l, 1); // dp[i] will store the size of the largest divisible subset ending at nums[i]
-        int max_index = 0; // Index of the last element of the largest subset
-        int max_size = 1; // Size of the largest subset
+        if (l == 0) return res;
+        vector<int> dp(l, 1); 
+        int max_index = 0; 
+        int max_size = 1; 
         for (int i = 1; i < l; i++) {
             for (int j = 0; j < i; j++) {
                 if (nums[i] % nums[j] == 0) {
@@ -19,8 +19,8 @@ public:
                 }
             }
         }
-        int curr_size = dp[max_index]; // Current size of the subset being constructed
-        int curr_num = nums[max_index]; // Current number being considered
+        int curr_size = max_size; 
+        int curr_num = nums[max_index]; 
         for (int i = max_index; i >= 0; i--) {
             if (curr_size == dp[i] && curr_num % nums[i] == 0) {
                 res.push_back(nums[i]);
