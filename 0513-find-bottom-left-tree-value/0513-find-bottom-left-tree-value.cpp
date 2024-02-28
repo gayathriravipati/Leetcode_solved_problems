32cb927b38d440ce9b8cd5e1be3e22d27e091093
Gayathri <gayathrir1104@gmail.com>
2024-02-28 09:03:31 -0500

    Time: 7 ms (79.35%), Space: 21.2 MB (40.76%) - LeetHub

--- 0513-find-bottom-left-tree-value/0513-find-bottom-left-tree-value.cpp
+++ 0513-find-bottom-left-tree-value/0513-find-bottom-left-tree-value.cpp
@@ -16,29 +16,31 @@ using namespace std;
 class Solution {
 public:
     int findBottomLeftValue(TreeNode* root) {
-        if (!root) return -1; 
-        
-        queue<TreeNode*> nodesQueue;
-        vector<vector<int>> levels; 
-        
-        nodesQueue.push(root); 
-        
-        while (!nodesQueue.empty()) {
-            int levelSize = nodesQueue.size();
-            vector<int> currentLevel;
-            
-            for (int i = 0; i < levelSize; ++i) {
-                TreeNode* current = nodesQueue.front();
-                nodesQueue.pop();
-                currentLevel.push_back(current->val);
-                
-                if (current->left) nodesQueue.push(current->left);
-                if (current->right) nodesQueue.push(current->right);
-            }
-            
-            levels.push_back(currentLevel); 
+        if(root == nullptr){
+            return -1;
         }
+        queue<TreeNode*>q;
+        vector<vector<int>>levels;
         
-        return levels.back()[0];
+        q.push(root);
+        
+        while(!q.empty()){
+            int l = q.size();
+            vector<int>cv;
+            
+            for(int i=0;i<l;i++){
+                TreeNode* current = q.front();
+                q.pop();
+                if(current->left){
+                    q.push(current->left);
+                }
+                if(current->right){
+                    q.push(current->right);
+                }
+                cv.push_back(current->val);
+            }
+            levels.push_back(cv);
+        }
+       return levels.back()[0];
     }
 };
