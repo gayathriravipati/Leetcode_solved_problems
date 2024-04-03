50dcc498a2dd0dd2f1160e7bc4d1655ade568a6e
Gayathri <gayathrir1104@gmail.com>
2024-04-02 23:47:57 -0400

    Time: 11 ms (58.82%), Space: 42.5 MB (37.63%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.java
+++ 0205-isomorphic-strings/0205-isomorphic-strings.java
@@ -1,22 +1,33 @@
-class Solution {
-    private String transformString(String s) {
-        Map<Character, Integer> indexMapping = new HashMap<>();
-        StringBuilder builder = new StringBuilder();
-        
-        for (int i = 0; i < s.length(); ++i) {
-            char c1 = s.charAt(i);
-            
-            if (!indexMapping.containsKey(c1)) {
-                indexMapping.put(c1, i);
-            }
-            
-            builder.append(Integer.toString(indexMapping.get(c1)));
-            builder.append(" ");
-        }
-        return builder.toString();
-    }
-    
+class Solution {    
     public boolean isIsomorphic(String s, String t) {
-        return transformString(s).equals(transformString(t));
+        Map<Character, Character> charmapping = new HashMap<>();
+        Set<Character> st = new HashSet<>();
+
+        
+        if(s.length() != t.length()){
+            return false;
+        }
+        
+        for (int i = 0; i < s.length(); i++) {
+            char c1 = s.charAt(i);
+            char c2 = t.charAt(i);
+            
+            if(charmapping.containsKey(c1)){
+                if(c2 != charmapping.get(c1)){
+                    return false;
+                }
+            }
+            else{
+                if(st.contains(c2)){
+                    return false;
+                }
+                else{
+                    charmapping.put(c1,c2);
+                    st.add(c2);
+                }
+            }
+        }
+        
+        return true;
     }
 }
