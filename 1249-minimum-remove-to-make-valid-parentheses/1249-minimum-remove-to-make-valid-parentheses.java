09177f46d60cfa05d8ff044ac1455ba5cafd206f
Gayathri <gayathrir1104@gmail.com>
2024-04-06 01:18:53 -0400

    Time: 13 ms (88.50%), Space: 45.1 MB (89.28%) - LeetHub

---
+++ 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.java
@@ -0,0 +1,43 @@
+class Solution {
+    public String minRemoveToMakeValid(String s) {
+        int open = 0;
+        int closed = 0;
+        StringBuilder sb = new StringBuilder();
+        
+        
+        for(int i=0;i<s.length();i++){
+            if(s.charAt(i)==')'){
+                if(open > 0){
+                    open--;
+                    sb.append(s.charAt(i));
+                    // System.out.println(sb + "" + "1");
+                }
+            }
+            else if(s.charAt(i)=='('){
+                open++;
+                sb.append(s.charAt(i));
+                // System.out.println(sb + "" + "2");
+            }
+            else{
+                sb.append(s.charAt(i));
+                // System.out.println(sb + "" + "3");
+            }
+        }
+        
+        // System.out.println(open);
+        
+        int i = sb.length() - 1; 
+        while (open > 0 && i >= 0) { 
+            if (sb.charAt(i) == '(') {
+                open--;
+                sb.deleteCharAt(i);
+            }
+            i--; 
+        }
+
+        
+        
+        return sb.toString();
+    }
+    
+}
