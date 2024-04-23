42143bd9ed0b4e5bc013c4526f5152cf8ed1db49
Gayathri <gayathrir1104@gmail.com>
2024-04-22 21:51:58 -0400

    Time: 0 ms (100.00%), Space: 44.7 MB (8.77%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.java
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.java
@@ -19,13 +19,13 @@ class Solution {
             return true;
         }
         
-        if(root.left != null && root.left.val >= root.val){
-            return false;
-        }
+//         if(root.left != null && root.left.val >= root.val){
+//             return false;
+//         }
         
-        if(root.right!=null && root.right.val <= root.val){
-            return false;
-        }
+//         if(root.right!=null && root.right.val <= root.val){
+//             return false;
+//         }
         
         
          if ((low != null && root.val <= low) || (high != null && root.val >= high)) {
