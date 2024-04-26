f5006cf498d3d6519cee025fd56ae8ad304ccea3
Gayathri <gayathrir1104@gmail.com>
2024-04-25 22:21:33 -0400

    Time: 0 ms (100.00%), Space: 47.7 MB (34.76%) - LeetHub

--- 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.java
+++ 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.java
@@ -14,25 +14,46 @@
  * }
  */
 class Solution {
+    public int findleftheight(TreeNode root){
+        if(root==null){
+            return 0;
+        }
+        return 1 + findleftheight(root.left);
+    }
+    
+    public int findrightheight(TreeNode root){
+        if(root==null){
+            return 0;
+        }
+        return 1 + findrightheight(root.right);
+    }
     
     public int countNodes(TreeNode root) {
-        Stack<TreeNode> st = new Stack<>();
         if (root == null) {
             return 0;
         }
-        st.push(root);
-        int c = 0;
-        while(!st.isEmpty()){
-            c++;
-            TreeNode node = st.peek();
-            st.pop();
-            if(node.left!=null){
-                st.push(node.left);
-            }
-            if(node.right!=null){
-                st.push(node.right);
-            }
+        int lh = findleftheight(root);
+        int rh = findrightheight(root);
+        
+        if(lh==rh){
+            return (int)Math.pow(2,lh)-1;
         }
-        return c;
+        
+        return 1+countNodes(root.left) + countNodes(root.right);
+        
+        // st.push(root);
+        // int c = 0;
+        // while(!st.isEmpty()){
+        //     c++;
+        //     TreeNode node = st.peek();
+        //     st.pop();
+        //     if(node.left!=null){
+        //         st.push(node.left);
+        //     }
+        //     if(node.right!=null){
+        //         st.push(node.right);
+        //     }
+        // }
+        // return c;
     }
 }
