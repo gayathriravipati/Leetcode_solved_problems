b7221412713fced9f27c54ba0b4d3487f5729fd5
Gayathri <gayathrir1104@gmail.com>
2024-07-16 14:49:01 -0400

    Time: 6 ms (60.89%) | Memory: 45.4 MB (29.15%) - LeetSync

---
+++ 735-asteroid-collision/asteroid-collision.java
@@ -0,0 +1,47 @@
+import java.util.Stack;
+
+class Solution {
+    public int[] asteroidCollision(int[] asteroids) {
+        Stack<Integer> stack = new Stack<>();
+        
+        for (int i = 0; i < asteroids.length; i++) {
+            if (stack.isEmpty()) {
+                stack.push(asteroids[i]);
+                continue;
+            }
+
+            while (true) {
+                int topVal = stack.peek();
+                // same direction or topVal is moving left and current asteroid is moving right
+                if ((topVal > 0 && asteroids[i] > 0) || (topVal < 0 && asteroids[i] < 0) || (topVal < 0 && asteroids[i] > 0)) {
+                    stack.push(asteroids[i]);
+                    break;
+                }
+                // opposite direction and same size
+                else if (Math.abs(topVal) == Math.abs(asteroids[i])) {
+                    stack.pop();
+                    break;
+                }
+                // opposite direction and topVal is larger
+                else if (Math.abs(topVal) > Math.abs(asteroids[i])) {
+                    break;
+                }
+                // opposite direction and topVal is smaller
+                else {
+                    stack.pop();
+                    if (stack.isEmpty()) {
+                        stack.push(asteroids[i]);
+                        break;
+                    }
+                }
+            }
+        }
+        
+        // Convert stack to array
+        int[] array = new int[stack.size()];
+        for (int i = stack.size() - 1; i >= 0; i--) {
+            array[i] = stack.get(i);
+        }
+        return array;
+    }
+}
