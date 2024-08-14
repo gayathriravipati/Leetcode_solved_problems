3d2a07dffa62258554a8ccaf7bfe8cb9ab7b2b6d
Gayathri <gayathrir1104@gmail.com>
2024-08-14 00:58:03 -0400

    Time: 4 ms (14.92%), Space: 44.8 MB (22.15%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.java
+++ 0695-max-area-of-island/0695-max-area-of-island.java
@@ -1,28 +1,56 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
 class Solution {
     int[][] grid;
     boolean[][] visited;
-    int maxArea = 0;
-
+    
     public int maxAreaOfIsland(int[][] grid) {
+        if (grid == null || grid.length == 0 || grid[0].length == 0) return 0;
+        
         this.grid = grid;
         int rows = grid.length;
-        int columns = grid[0].length;
-        visited = new boolean[rows][columns]; 
+        int cols = grid[0].length;
+        this.visited = new boolean[rows][cols];
+        int maxArea = 0;
+        
+        // Iteration through the grid
         for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < columns; j++) {
-                if (grid[i][j] == 1 && !visited[i][j]) {  
-                    maxArea = Math.max(maxArea, dfs(i, j));
+            for (int j = 0; j < cols; j++) {
+                if (!visited[i][j] && grid[i][j] == 1) {
+                    maxArea = Math.max(maxArea, explore(i, j));
                 }
             }
         }
-        return maxArea;
+        return maxArea; 
     }
-
-    public int dfs(int i, int j) {
-        if (i < 0 || i >= grid.length || j < 0 || j >= grid[0].length || grid[i][j] == 0 || visited[i][j]) {
-            return 0;
+    
+    private int explore(int r, int c) {
+        int[][] directions = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+        Queue<int[]> queue = new LinkedList<>();
+        queue.add(new int[]{r, c});
+        int totalCells = 0;
+        int rows = grid.length;
+        int cols = grid[0].length;
+        visited[r][c] = true;
+        
+        while (!queue.isEmpty()) {
+            totalCells++;
+            int[] current = queue.poll();
+            int currRow = current[0];
+            int currCol = current[1];
+            
+            for (int[] dir : directions) {
+                int newRow = currRow + dir[0];
+                int newCol = currCol + dir[1];
+                
+                if (newRow >= 0 && newRow < rows && newCol >= 0 && newCol < cols 
+                    && grid[newRow][newCol] == 1 && !visited[newRow][newCol]) {
+                    queue.add(new int[]{newRow, newCol});
+                    visited[newRow][newCol] = true;
+                }
+            }
         }
-        visited[i][j] = true; 
-        return 1 + dfs(i - 1, j) + dfs(i + 1, j) + dfs(i, j - 1) + dfs(i, j + 1);
+        return totalCells;
     }
 }
