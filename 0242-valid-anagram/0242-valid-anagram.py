8774f1fecbaaeb33696072303e4eeb6dc962abbc
Gayathri <gayathrir1104@gmail.com>
2024-08-15 18:30:47 -0400

    Time: 56 ms (22.64%), Space: 17.6 MB (7.30%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,43 +1,12 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        # freq of each char in s
-        # freq of each char in t
-        # iterate through t_dict and check the value of key in s is matching
-        s_dict = {}
-        t_dict = {}
+        sorted_chars = sorted(s)
+        sorted_s_string = ''.join(sorted_chars) 
         
-        for i in range(len(s)):
-            if s[i] in s_dict:
-                s_dict[s[i]] += 1
-            else:
-                s_dict[s[i]] = 1
+        sorted_chars = sorted(t)
+        sorted_t_string = ''.join(sorted_chars) 
         
-        
-        for i in range(len(t)):
-            if t[i] in t_dict:
-                t_dict[t[i]] =  t_dict[t[i]] + 1
-            else:
-                t_dict[t[i]] = 1
-        
-        
-        for key in t_dict:
-            if key in s_dict:
-                if t_dict[key] == s_dict[key]:
-                    continue
-                else:
-                    return False
-            else:
-                return False
-            
-        for key in s_dict:
-            if key in t_dict:
-                if s_dict[key] == t_dict[key]:
-                    continue
-                else:
-                    return False
-            else:
-                return False
-            
-        
-        return True
-                
+        if sorted_s_string == sorted_t_string:
+            return True
+        else:
+            return False
