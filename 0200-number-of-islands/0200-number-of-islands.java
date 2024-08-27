f290ebf0d57f7ba44e0802502ef9ae002653696e
Gayathri <gayathrir1104@gmail.com>
2024-08-27 18:46:11 -0500

    Time: 6 ms (23.45%), Space: 52.2 MB (12.82%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -1,29 +1,47 @@
 class Solution {
     char[][] grid;
     boolean[][] visited;
-    int c = 0;
+    int rows;
+    int cols;
+    
     public int numIslands(char[][] grid) {
         this.grid = grid;
-        visited = new boolean[grid.length][grid[0].length];
-        for(int i = 0;i<grid.length;i++){
-            for(int j=0;j< grid[0].length;j++){
-                if(grid[i][j]=='1' && !visited[i][j]){
-                    c++;
-                    dfs(i,j);
+        this.rows = grid.length;
+        this.cols = grid[0].length;
+        visited = new boolean[rows][cols];
+        int totalIslands = 0;
+        
+        for(int i=0; i<rows; i++){
+            for(int j=0; j<cols; j++){
+                if(grid[i][j] == '1' && !visited[i][j]){
+                    totalIslands++;
+                    explore(i,j);
                 }
             }
         }
-        return c;
+        return totalIslands;
     }
     
-    public void dfs(int i, int j){
-        if(i < 0 || j < 0 || i >= grid.length || j >= grid[0].length || grid[i][j] == '0' || visited[i][j]){
-            return;
+    private void explore(int r, int c){
+        visited[r][c] = true;
+        Queue<int[]> q = new LinkedList<>();
+        q.add(new int[]{r,c});
+        int[][] dirs = {{1,0}, {0,1}, {-1,0}, {0,-1}};
+        
+        while(!q.isEmpty()){
+            int[] current = q.poll();
+            int currentRow = current[0];
+            int currentCol = current[1];
+            
+            for(int i=0; i<dirs.length; i++){
+                int newRow = currentRow + dirs[i][0];
+                int newCol = currentCol + dirs[i][1];
+                if(newRow >= 0 && newRow <= rows-1 && newCol>=0 && newCol <=cols-1 && !visited[newRow][newCol] && grid[newRow][newCol] == '1'){
+                    q.add(new int[]{newRow,newCol});
+                    visited[newRow][newCol] = true;
+                }
+            }
         }
-        visited[i][j] = true;
-        dfs(i+1,j);
-        dfs(i,j+1);
-        dfs(i-1,j);
-        dfs(i,j-1);
     }
+    
 }
