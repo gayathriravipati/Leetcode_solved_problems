98a887ba4e08d6026a89971540aa0be6d9153682
Gayathri <gayathrir1104@gmail.com>
2024-08-27 09:46:30 -0500

    Time: 4 ms (14.85%), Space: 44.7 MB (39.89%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.java
+++ 0695-max-area-of-island/0695-max-area-of-island.java
@@ -1,8 +1,8 @@
 class Solution {
     int[][] grid;
-    boolean[][] visited;
     int rows;
     int cols;
+    boolean[][] visited;
     
     public int maxAreaOfIsland(int[][] grid) {
         this.grid = grid;
@@ -10,42 +10,39 @@ class Solution {
         this.cols = grid[0].length;
         visited = new boolean[rows][cols];
         int maxArea = 0;
-        
+        //iteration
         for(int i=0; i<rows; i++){
             for(int j=0; j<cols; j++){
-                if(!visited[i][j] && grid[i][j] == 1){
+                if(grid[i][j]==1 && !visited[i][j]){
                     maxArea = Math.max(maxArea, explore(i,j));
                 }
             }
         }
-        
         return maxArea;
     }
     
     private int explore(int r, int c){
         visited[r][c] = true;
-        Queue<int[]> queue = new LinkedList<>();
-        queue.add(new int[]{r,c});
+        int[][] dirs = {{1,0}, {0,1}, {-1,0}, {0,-1}};
+        Queue<int[]> q = new LinkedList<>();
+        q.add(new int[]{r, c});
         int totalCells = 0;
-        int[][] dirs = {{1,0},{-1,0},{0,1},{0,-1}};
         
-        while(!queue.isEmpty()){
+        while(!q.isEmpty()){
             totalCells++;
-            int[] current = queue.poll();
-            int currRow = current[0];
-            int currCol = current[1];
-            
-            for(int i = 0; i<dirs.length; i++){
-                int newRow = currRow + dirs[i][0];
-                int newCol = currCol + dirs[i][1];
-                
-                if(newRow >= 0 && newRow < rows && newCol >=0 && newCol < cols && grid[newRow][newCol] == 1 && !visited[newRow][newCol]){
-                    queue.add(new int[]{newRow,newCol});
+            int[] current = q.poll();
+            int currentRow = current[0];
+            int currentCol = current[1];
+            for(int i=0; i<dirs.length; i++){
+                int newRow = currentRow + dirs[i][0];
+                int newCol = currentCol + dirs[i][1];
+                if(newRow >= 0 && newRow<= rows-1 && newCol>=0 && newCol<=cols-1 && grid[newRow][newCol]==1 && !visited[newRow][newCol]){
+                    q.add(new int[]{newRow, newCol});
                     visited[newRow][newCol] = true;
                 }
-            }   
+            }
         }
-        
         return totalCells;
     }
+    
 }
