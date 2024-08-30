9935ef3f291d5b7b3de4a94725ea76662e94301d
Gayathri <gayathrir1104@gmail.com>
2024-08-30 13:01:04 -0500

    Time: 2 ms (51.81%), Space: 47.1 MB (73.00%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.java
+++ 0547-number-of-provinces/0547-number-of-provinces.java
@@ -1,42 +1,41 @@
+import java.util.*;
+
 class Solution {
     public int findCircleNum(int[][] isConnected) {
-        List<List<Integer>> neighbors = new ArrayList<>();
-        
-        for (int i = 0; i < isConnected.length; i++) {
-            neighbors.add(new ArrayList<>());
+        int n = isConnected.length;
+        boolean[] visited = new boolean[n];
+        int totalProvinces = 0;
+        List<Integer>[] adjacencylist = new ArrayList[n];
+
+        for (int i = 0; i < n; i++) {
+            adjacencylist[i] = new ArrayList<>();
         }
-        
-        for(int i=0; i<isConnected.length; i++){
-            for(int j=i+1; j<isConnected[0].length; j++){
-                if(isConnected[i][j] == 1){
-                    neighbors.get(i).add(j);
-                    neighbors.get(j).add(i);
+
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {  
+                if (isConnected[i][j] == 1) {
+                    adjacencylist[i].add(j);
+                    adjacencylist[j].add(i);
                 }
             }
         }
-        
-        int totalProvinces = 0;
-        Set<Integer> visited = new HashSet<>();
-        
-        
-        for(int i = 0; i<neighbors.size(); i++){
-            if(!visited.contains(i)){
+
+        for (int i = 0; i < n; i++) {
+            if (!visited[i]) { 
                 totalProvinces++;
-                dfs(i,neighbors,visited);
+                explore(i, visited, adjacencylist);
             }
         }
-        
+
         return totalProvinces;
     }
-    
-    public void dfs(int i, List<List<Integer>> neighbors, Set<Integer> visited){
-        visited.add(i);
-        for(int j = 0; j<neighbors.get(i).size(); j++){
-            if(!visited.contains(neighbors.get(i).get(j))){
-                visited.add(neighbors.get(i).get(j));
-                dfs(neighbors.get(i).get(j),neighbors,visited);
+
+    public void explore(int idx, boolean[] visited, List<Integer>[] adjacencylist) {
+        visited[idx] = true;
+        for (int neighbor : adjacencylist[idx]) {  
+            if (!visited[neighbor]) {
+                explore(neighbor, visited, adjacencylist);
             }
         }
     }
-    
 }
