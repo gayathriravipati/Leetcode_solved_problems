1cde2f2ac0369c9deb97d36dcb662b0e9196690e
Gayathri <gayathrir1104@gmail.com>
2024-09-06 17:43:30 -0500

    Time: 46 ms (63.17%) | Memory: 116.4 MB (32.64%) - LeetSync

--- 146-lru-cache/lru-cache.java
+++ 146-lru-cache/lru-cache.java
@@ -1,43 +1,68 @@
-class LRUCache {
-    Deque<Integer> dq;
-    HashMap<Integer, Integer> mapper;
-    int capacity;
-    public LRUCache(int capacity) {
-        dq = new ArrayDeque<>(capacity);
-        mapper = new HashMap<>();
-        this.capacity = capacity;
-    }
-    
-    public int get(int key) {
-        if(mapper.containsKey(key)){
-            dq.remove(key);
-            dq.addLast(key);
-            return mapper.get(key);
-        }
-        else{
-            return -1;
-        }
-    }
-    
-    public void put(int key, int value) {
-        
-        if(mapper.containsKey(key)){
-            dq.remove(key);
-        }
-        else{
-            if((mapper.size() == capacity)){
-                int oldestVal = dq.removeFirst();
-                mapper.remove(oldestVal);
-            }
-        }
-        dq.addLast(key);
-        mapper.put(key, value);
+class ListNode {
+    int key;
+    int val;
+    ListNode next;
+    ListNode prev;
+
+    public ListNode(int key, int val) {
+        this.key = key;
+        this.val = val;
     }
 }
 
-/**
- * Your LRUCache object will be instantiated and called as such:
- * LRUCache obj = new LRUCache(capacity);
- * int param_1 = obj.get(key);
- * obj.put(key,value);
- */
+class LRUCache {
+    int capacity;
+    Map<Integer, ListNode> dic;
+    ListNode head;
+    ListNode tail;
+
+    public LRUCache(int capacity) {
+        this.capacity = capacity;
+        dic = new HashMap<>();
+        head = new ListNode(-1, -1);
+        tail = new ListNode(-1, -1);
+        head.next = tail;
+        tail.prev = head;
+    }
+
+    public int get(int key) {
+        if (!dic.containsKey(key)) {
+            return -1;
+        }
+
+        ListNode node = dic.get(key);
+        remove(node);
+        add(node);
+        return node.val;
+    }
+
+    public void put(int key, int value) {
+        if (dic.containsKey(key)) {
+            ListNode oldNode = dic.get(key);
+            remove(oldNode);
+        }
+
+        ListNode node = new ListNode(key, value);
+        dic.put(key, node);
+        add(node);
+
+        if (dic.size() > capacity) {
+            ListNode nodeToDelete = head.next;
+            remove(nodeToDelete);
+            dic.remove(nodeToDelete.key);
+        }
+    }
+
+    public void add(ListNode node) {
+        ListNode previousEnd = tail.prev;
+        previousEnd.next = node;
+        node.prev = previousEnd;
+        node.next = tail;
+        tail.prev = node;
+    }
+
+    public void remove(ListNode node) {
+        node.prev.next = node.next;
+        node.next.prev = node.prev;
+    }
+}
