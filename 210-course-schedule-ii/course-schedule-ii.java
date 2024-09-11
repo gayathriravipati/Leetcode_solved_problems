773f0dd71a5242fceac21bbb5d838a1ae320348c
Gayathri <gayathrir1104@gmail.com>
2024-09-11 11:07:39 -0500

    Time: 4 ms (91.39%) | Memory: 45.6 MB (70.96%) - LeetSync

--- 210-course-schedule-ii/course-schedule-ii.java
+++ 210-course-schedule-ii/course-schedule-ii.java
@@ -2,14 +2,12 @@ import java.util.*;
 
 class Solution {
     public int[] findOrder(int numCourses, int[][] prerequisites) {
-        // Initialize the adjacency list
         List<Integer>[] adjacencyList = new ArrayList[numCourses];
 
-        for (int i = 0; i < numCourses; i++) {  // Corrected numCourses.length to numCourses
+        for (int i = 0; i < numCourses; i++) { 
             adjacencyList[i] = new ArrayList<>();
         }
 
-        // Build the adjacency list for the graph
         for (int i = 0; i < prerequisites.length; i++) {
             adjacencyList[prerequisites[i][1]].add(prerequisites[i][0]);
         }
@@ -18,19 +16,16 @@ class Solution {
         Set<Integer> visited = new HashSet<>();
         Set<Integer> inStack = new HashSet<>();
 
-        // Perform DFS to detect cycles and find topological order
         for (int i = 0; i < numCourses; i++) {
             if (!visited.contains(i)) {
                 if (!dfs(i, visited, inStack, adjacencyList, order)) {
-                    return new int[0];  // Return empty array if a cycle is detected
+                    return new int[0];  
                 }
             }
         }
 
-        // Convert the List<Integer> to Integer[]
         Integer[] integerArray = order.toArray(new Integer[0]);
 
-        // Convert Integer[] to int[]
         int[] result = new int[integerArray.length];
         int j = 0;
         for (int i = integerArray.length-1; i >=0; i--) {
@@ -41,25 +36,22 @@ class Solution {
     }
 
     private boolean dfs(int node, Set<Integer> visited, Set<Integer> inStack, List<Integer>[] adjacencyList, List<Integer> order) {
-        visited.add(node);  // Mark the current node as visited
-        inStack.add(node);  // Add it to the current recursion stack
-
+        visited.add(node);  
+        inStack.add(node);  
         List<Integer> neighbors = adjacencyList[node];
-
-        // Traverse all neighbors
         for (int neighbor : neighbors) {
-            if (inStack.contains(neighbor)) {  // Cycle detected
+            if (inStack.contains(neighbor)) {  
                 return false;
             }
             if (!visited.contains(neighbor)) {
                 if (!dfs(neighbor, visited, inStack, adjacencyList, order)) {
-                    return false;  // If a cycle is detected in any DFS call, return false
+                    return false; 
                 }
             }
         }
 
-        inStack.remove(node);  // Remove the node from the recursion stack
-        order.add(node);  // Add the current node to the topological order
+        inStack.remove(node);  
+        order.add(node); 
 
         return true;
     }
@@ -68,6 +60,6 @@ class Solution {
         Solution sol = new Solution();
         int numCourses = 2;
         int[][] prerequisites = {{1, 0}, {0, 1}};
-        System.out.println(Arrays.toString(sol.findOrder(numCourses, prerequisites)));  // Expected output: []
+        System.out.println(Arrays.toString(sol.findOrder(numCourses, prerequisites)));  
     }
 }
