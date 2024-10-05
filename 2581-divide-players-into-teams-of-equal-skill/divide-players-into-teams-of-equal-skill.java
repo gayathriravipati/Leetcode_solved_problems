dfac73df88f2fd1e5d415a80338e9b94c148bb14
Gayathri <gayathrir1104@gmail.com>
2024-10-05 14:03:02 -0500

    Time: 3 ms (96.50%) | Memory: 55.5 MB (94.25%) - LeetSync

--- 2581-divide-players-into-teams-of-equal-skill/divide-players-into-teams-of-equal-skill.java
+++ 2581-divide-players-into-teams-of-equal-skill/divide-players-into-teams-of-equal-skill.java
@@ -1,22 +1,38 @@
 class Solution {
+
     public long dividePlayers(int[] skill) {
-        int len = skill.length;
-        Arrays.sort(skill);
-        int globalChem = skill[0] + skill[len-1];
-        System.out.println(globalChem);
-        int i=0, j = len -1;
-        long compSum = 0;
-        
-        while(i < j){
-            if(skill[i] + skill[j] == globalChem){
-                compSum += (skill[i] * skill[j]);
-            }
-            else{
+        int n = skill.length;
+        int totalSkill = 0;
+        int[] skillFrequency = new int[1001];
+
+        // Calculate total skill and skill frequency
+        for (int playerSkill : skill) {
+            totalSkill += playerSkill;
+            skillFrequency[playerSkill]++;
+        }
+
+        // Check if total skill can be evenly distributed among teams
+        if (totalSkill % (n / 2) != 0) {
+            return -1;
+        }
+
+        int targetTeamSkill = totalSkill / (n / 2);
+        long totalChemistry = 0;
+
+        // Calculate total chemistry while verifying valid team formations
+        for (int playerSkill : skill) {
+            int partnerSkill = targetTeamSkill - playerSkill;
+
+            // Check if a valid partner exists
+            if (skillFrequency[partnerSkill] == 0) {
                 return -1;
             }
-            i++;
-            j--;
+
+            totalChemistry += (long) playerSkill * (long) partnerSkill;
+            skillFrequency[partnerSkill]--;
         }
-        return compSum;
+
+        // Return half of totalChemistry as each pair is counted twice
+        return totalChemistry / 2;
     }
 }
