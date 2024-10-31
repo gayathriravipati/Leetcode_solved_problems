5da83e80c458d7292b35d9e47c3b16748dbc5117
Gayathri <gayathrir1104@gmail.com>
2024-10-30 21:18:31 -0500

    Time: 24 ms (49.33%), Space: 45.4 MB (76.88%) - LeetHub

--- 0621-task-scheduler/0621-task-scheduler.java
+++ 0621-task-scheduler/0621-task-scheduler.java
@@ -1,31 +1,36 @@
 class Solution {
     public int leastInterval(char[] tasks, int n) {
         int[] freq = new int[26];
-        for (char ch : tasks) {
+        for(char ch : tasks){
             freq[ch - 'A']++;
         }
         
         PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
-        for (int i = 0; i < 26; i++) {
-            if (freq[i] > 0) {
-                pq.offer(freq[i]);
+        for(int i : freq){
+            if(i > 0){
+                pq.add(i);
             }
         }
-
+        
         int time = 0;
-        while (!pq.isEmpty()) {
-            int cycle = n + 1;
+        while(!pq.isEmpty()){
+            int cycle = n+1;
             List<Integer> store = new ArrayList<>();
             int taskCount = 0;
-            while (cycle-- > 0 && !pq.isEmpty()) {
-                int currentFreq = pq.poll();
-                if (currentFreq > 1) {
-                    store.add(currentFreq - 1);
+            
+            while(cycle-- > 0 && !pq.isEmpty()){
+                int val = pq.poll();
+                if(val > 1){
+                    store.add(val-1);
                 }
                 taskCount++;
             }
-            store.forEach(pq::offer);
-            time += (pq.isEmpty() ? taskCount : n + 1);
+            
+            if(store.size() > 0){
+                store.forEach(pq::offer);
+            }
+            
+            time += (pq.isEmpty() ? taskCount : n + 1);   
         }
         return time;
     }
