31132e88fb2592c192b6287a0cc847c05e00f62a
Gayathri <gayathrir1104@gmail.com>
2024-11-02 11:17:16 -0500

    Time: 1 ms (80.83%) | Memory: 41.6 MB (75.61%) - LeetSync

--- 91-decode-ways/decode-ways.java
+++ 91-decode-ways/decode-ways.java
@@ -1,21 +1,46 @@
 class Solution {
-    public int numDecodings(String s) {
-        int n = s.length();
-        int[] dp = new int[n+1];
-        dp[n] = 1;
+//     public int numDecodings(String s) {
+//         int[] dp = new int[s.length() + 1];
+//         dp[0] = 1;
         
-        for(int i = n-1; i>=0; i--){
-            if(s.charAt(i) == '0'){
-                dp[i] = 0;
-            }
-            else{
-                dp[i] = dp[i+1];
-                if (i + 1 < n && (s.charAt(i) == '1' || (s.charAt(i) == '2' && "0123456".indexOf(s.charAt(i + 1)) != -1))) {
-                    dp[i] += dp[i + 2];  
-                }
-            }
-    }
-     return dp[0];
-}
+//         dp[1] = s.charAt(0) == '0' ? 0 : 1;
+        
+//         for(int i=2; i<dp.length; i++){
+//             dp[i] = s.charAt(i-1) == '0' ? 0 : dp[i-1];
+            
+//             int val = Integer.valueOf(s.substring(i-2,i));
+            
+//             if(val >= 10 && val <= 26){
+//                 dp[i] += dp[i-2];
+//             }
+            
+//         }
+//         return dp[s.length()];
+//     }
     
+    public int numDecodings(String s) {
+        
+        if (s == null || s.length() == 0 || s.charAt(0) == '0') {
+                return 0;
+        }
+        
+        int twoBack = 1;
+        int oneBack = s.charAt(0) == '0' ? 0 : 1;
+        int current = 0;
+        
+        for(int i=2; i<=s.length(); i++){
+            current = s.charAt(i-1) == '0' ? 0 : oneBack;
+            
+            int val = Integer.valueOf(s.substring(i-2,i));
+            
+            if(val >= 10 && val <= 26){
+                current += twoBack;
+            }
+            
+            twoBack = oneBack;
+            oneBack = current;
+        }
+        
+        return oneBack;
+    }
 }
