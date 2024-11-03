60b845c2d690363ae67c2e7865ca9eba1d3649f0
Gayathri <gayathrir1104@gmail.com>
2024-11-02 19:23:10 -0500

    Time: 6 ms (97.54%), Space: 47.7 MB (54.85%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -1,22 +1,19 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
+        
+        Map<String, List<String>> anagrams = new HashMap<>();
+        
+        for(String str : strs){
+            char[] currentString = str.toCharArray();
+            Arrays.sort(currentString);
+            String sortedStr = new String(currentString);
+            anagrams.computeIfAbsent(sortedStr, k -> new ArrayList<>()).add(str);
+        }
+        
         List<List<String>> res = new ArrayList<>();
-        Map<String, List<String>> anagramMapper = new HashMap<>();
         
-        if(strs.length == 0){
-            return res;
-        }
-        
-        for(int i=0; i<strs.length; i++){
-            String s = strs[i];
-            char[] charArray = s.toCharArray();
-            Arrays.sort(charArray);
-            String sortedStr = new String(charArray);
-            anagramMapper.computeIfAbsent(sortedStr, k->new ArrayList<>()).add(s);
-        }
-        
-        for(List<String> mapper : anagramMapper.values()){
-            res.add(mapper);
+        for(List<String> str : anagrams.values()){
+            res.add(str);
         }
         
         return res;
