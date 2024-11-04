01a78e200acaa1f85f9fb5344b5a1d040ec2dd7f
Gayathri <gayathrir1104@gmail.com>
2024-11-04 00:01:36 -0600

    Time: 6 ms (74.41%) | Memory: 41.5 MB (64.66%) - LeetSync

--- 647-palindromic-substrings/palindromic-substrings.java
+++ 647-palindromic-substrings/palindromic-substrings.java
@@ -1,33 +1,39 @@
 class Solution {
     public int countSubstrings(String s) {
-//         int maxLen = 0;
-//         String res = "";
-        int cnt = 0;
+        if(s.length() == 1){
+            return 1;
+        }
+        
+        int totalPalin = 0;
         
         for(int i=0; i<s.length(); i++){
-            int start=i, end = i;
-            
-            while(start >= 0 && end < s.length() && s.charAt(start) == s.charAt(end)){
-               // if(maxLen < end-start+1){
-               //     maxLen = end-start+1;
-               //     res = s.substring(start, end+1);
-               // }
-               cnt++;
-               start--;
-               end++;
+            totalPalin += countpalin(s, i, i);
+            // System.out.println(totalPalin);
+        }
+        
+        for(int i=0; i<s.length(); i++){
+            totalPalin += countpalin(s, i, i+1);
+        }
+        
+        return totalPalin;
+    }
+    
+    public int countpalin(String s, int start, int end){
+        int palin = 0;
+        
+        while(start >= 0 && end < s.length()){
+            if(s.charAt(start) == s.charAt(end)){
+                palin++;
+                // System.out.println(start + " " + end + " " + palin);
+                start--;
+                end++;
             }
-            
-            start = i; end = i+1;
-            while(start >= 0 && end < s.length() && s.charAt(start) == s.charAt(end)){
-               // if(maxLen < end-start+1){
-               //     maxLen = end-start+1;
-               //     res = s.substring(start, end+1);
-               // }
-               cnt++;
-               start--;
-               end++;
+            else{
+                break;
             }
         }
-        return cnt;
+        
+        return palin;
     }
+    
 }
