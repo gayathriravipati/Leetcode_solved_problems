aaf28c9ead803fac907b9590dec5629c638e779f
Gayathri <gayathrir1104@gmail.com>
2024-11-07 06:19:11 -0600

    Time: 21 ms (7.03%), Space: 52.1 MB (16.53%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -1,47 +1,52 @@
 class Solution {
-    char[][] grid;
-    boolean[][] visited;
-    int rows;
-    int cols;
+        char[][] grid;
+        int rows;
+        int cols;
+        boolean[][] visited;
     
     public int numIslands(char[][] grid) {
         this.grid = grid;
         this.rows = grid.length;
         this.cols = grid[0].length;
-        visited = new boolean[rows][cols];
+        this.visited = new boolean[rows][cols];
         int totalIslands = 0;
         
         for(int i=0; i<rows; i++){
             for(int j=0; j<cols; j++){
                 if(grid[i][j] == '1' && !visited[i][j]){
                     totalIslands++;
-                    explore(i,j);
-                }
+                    explore(i, j);
+                } 
             }
         }
         return totalIslands;
     }
     
-    private void explore(int r, int c){
-        visited[r][c] = true;
+    public void explore(int i, int j){
+        
+        int[][] dirs = new int [][] {{1,0}, {0,1}, {-1,0}, {0,-1}};
         Queue<int[]> q = new LinkedList<>();
-        q.add(new int[]{r,c});
-        int[][] dirs = {{1,0}, {0,1}, {-1,0}, {0,-1}};
+        q.add(new int[] {i, j});
+        
+        System.out.println(grid[i][j]);
         
         while(!q.isEmpty()){
             int[] current = q.poll();
-            int currentRow = current[0];
-            int currentCol = current[1];
+            int currentX = current[0];
+            int currentY = current[1];
             
-            for(int i=0; i<dirs.length; i++){
-                int newRow = currentRow + dirs[i][0];
-                int newCol = currentCol + dirs[i][1];
-                if(newRow >= 0 && newRow <= rows-1 && newCol>=0 && newCol <=cols-1 && !visited[newRow][newCol] && grid[newRow][newCol] == '1'){
-                    q.add(new int[]{newRow,newCol});
-                    visited[newRow][newCol] = true;
-                }
+            for(int[] dir : dirs){
+                int newdirX = currentX + dir[0];
+                int newdirY = currentY + dir[1];
+                
+                if(newdirX >=0 && newdirX<rows && newdirY>=0 && newdirY<cols){
+                    if(grid[newdirX][newdirY] == '1' && !visited[newdirX][newdirY]){
+                        visited[newdirX][newdirY] = true;
+                        q.add(new int[] {newdirX, newdirY});
+                    }
+                }  
+            }
+            
             }
         }
-    }
-    
 }
