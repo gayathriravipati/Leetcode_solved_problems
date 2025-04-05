863d6ecf2da00d5e365d99f16883d898a7662179
Gayathri <gayathrir1104@gmail.com>
2025-04-04 19:17:23 -0500

    Time: 1 ms (32.2%), Space: 44.6 MB (18.89%) - LeetHub

--- 1218-lowest-common-ancestor-of-deepest-leaves/1218-lowest-common-ancestor-of-deepest-leaves.java
+++ 1218-lowest-common-ancestor-of-deepest-leaves/1218-lowest-common-ancestor-of-deepest-leaves.java
@@ -13,26 +13,30 @@
  *     }
  * }
  */
+ //learnt from editorial and techdose
 class Solution {
-
     public TreeNode lcaDeepestLeaves(TreeNode root) {
         return dfs(root).getKey();
     }
 
-    private Pair<TreeNode, Integer> dfs(TreeNode root) {
-        if (root == null) {
+    public Pair<TreeNode, Integer> dfs(TreeNode root){
+        if(root == null){
             return new Pair<>(null, 0);
         }
 
-        Pair<TreeNode, Integer> left = dfs(root.left);
-        Pair<TreeNode, Integer> right = dfs(root.right);
+        Pair<TreeNode, Integer> leftPair = dfs(root.left);
+        Pair<TreeNode, Integer> rightPair = dfs(root.right);
 
-        if (left.getValue() > right.getValue()) {
-            return new Pair<>(left.getKey(), left.getValue() + 1);
+        if(leftPair.getValue() > rightPair.getValue()){
+            return new Pair<>(leftPair.getKey(), leftPair.getValue() + 1);
         }
-        if (left.getValue() < right.getValue()) {
-            return new Pair<>(right.getKey(), right.getValue() + 1);
+
+        else if(rightPair.getValue() > leftPair.getValue()){
+            return new Pair<>(rightPair.getKey(), rightPair.getValue() + 1);
         }
-        return new Pair<>(root, left.getValue() + 1);
+
+        else{
+            return new Pair<>(root, leftPair.getValue() + 1);
+        }
     }
 }
