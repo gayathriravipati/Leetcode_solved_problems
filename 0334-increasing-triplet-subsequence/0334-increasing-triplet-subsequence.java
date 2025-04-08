4fb10ab646ba799cd9c5f24905906c6fb0a3da78
Gayathri <gayathrir1104@gmail.com>
2025-04-08 18:30:04 -0500

    Time: 2 ms (95.17%), Space: 133.5 MB (89.82%) - LeetHub

--- 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.java
+++ 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.java
@@ -1,17 +1,18 @@
 class Solution {
     //Learnt from techdose and editorial
+    //5 7 3 9 -- technically, the code is not guaranteeing strict i < j < k if you're being ultra-picky about positions when variables are updated out of order. This results true for the subsequence - 7 3 9 => 3 7 9
+
+
     public boolean increasingTriplet(int[] nums) {
         int firstVariable = Integer.MAX_VALUE;
         int secondVariable = Integer.MAX_VALUE;
-        
-        for(int i = 0; i<nums.length; i++){
-            if(nums[i] <= firstVariable){
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] <= firstVariable) {
                 firstVariable = nums[i];
-            }
-            else if(nums[i] <= secondVariable){
+            } else if (nums[i] <= secondVariable) {
                 secondVariable = nums[i];
-            }
-            else{
+            } else {
                 return true;
             }
         }
