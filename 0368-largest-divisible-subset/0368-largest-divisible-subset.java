1e9f17ad061a94b692057f3e4b52401cb355a867
Gayathri <gayathrir1104@gmail.com>
2025-04-08 19:40:36 -0500

    Time: 23 ms (7.74%), Space: 45 MB (6.89%) - LeetHub

--- 0368-largest-divisible-subset/0368-largest-divisible-subset.java
+++ 0368-largest-divisible-subset/0368-largest-divisible-subset.java
@@ -1,27 +1,40 @@
 class Solution {
-  public List<Integer> largestDivisibleSubset(int[] nums) {
-    int n = nums.length;
-    if (n == 0) return new ArrayList();
-        
-    List<ArrayList> EDS = new ArrayList();
-    for (int num : nums) EDS.add(new ArrayList());
+    //Learnt from meetcode and editorial
+    public List<Integer> largestDivisibleSubset(int[] nums) {
+        int len = nums.length;
 
-    Arrays.sort(nums);
+        if(len == 0){
+            return new ArrayList<>();
+        }
 
-    for (int i = 0; i < n; ++i) {
-      List<Integer> maxSubset = new ArrayList();
-            
-      for (int k = 0; k < i; ++k) 
-        if (nums[i] % nums[k] == 0 && maxSubset.size() < EDS.get(k).size())
-          maxSubset = EDS.get(k);
-          
-      EDS.get(i).addAll(maxSubset);
-      EDS.get(i).add(nums[i]);
+        List<List<Integer>> divisibleStack = new ArrayList<>();
+
+        for(int i=0; i<len; i++){
+            divisibleStack.add(new ArrayList<>());
+        }
+
+        Arrays.sort(nums);
+        int n = len;
+
+        for(int i = n-1; i>=0; i--){
+            List<Integer> tempStack = new ArrayList<>();
+            for(int j = i+1; j<n; j++){
+                if((nums[j] % nums[i] == 0) && (divisibleStack.get(j).size() > tempStack.size())){
+                    tempStack = divisibleStack.get(j);
+                }
+            }
+            divisibleStack.get(i).addAll(tempStack);
+            divisibleStack.get(i).add(nums[i]);
+        }
+
+        List<Integer> largestSubset = new ArrayList<>();
+
+        for(List<Integer> t : divisibleStack){
+            if(t.size() > largestSubset.size()){
+                largestSubset = t;
+            }
+        }
+
+        return largestSubset;
     }
-    
-    List<Integer> ret = new ArrayList();
-    for (int i = 0; i < n; ++i) 
-      if (ret.size() < EDS.get(i).size()) ret = EDS.get(i);
-    return ret;
-  }  
 }
