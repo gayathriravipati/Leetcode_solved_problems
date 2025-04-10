5697bf07238d2588af2697261983335ccf709335
Gayathri <gayathrir1104@gmail.com>
2025-04-10 11:01:15 -0500

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,15 +1,22 @@
 class Solution {
     public int climbStairs(int n) {
+
         if(n == 1){
             return 1;
         }
-        int[] dp = new int[n+1];
-        dp[1] = 1;
-        dp[2] = 2;
-        
-        for(int i=3; i<=n; i++){
-            dp[i] = dp[i-1] + dp[i-2];
+
+        if(n==2){
+            return 2;
         }
-        return dp[n];
+
+        int[] dp = new int[n+1];
+        dp[n] = 0;
+        dp[n-1] = 1;
+
+        for(int i = n-2; i>=0; i--){
+            dp[i] = 1 + Math.max(dp[i+1], dp[i+2]);
+        }
+
+        return dp[0];
     }
 }
