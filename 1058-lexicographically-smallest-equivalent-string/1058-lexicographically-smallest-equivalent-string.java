0427516c502fb174d10cb201a3be441909800aba
Gayathri <gayathrir1104@gmail.com>
2025-06-05 16:28:24 -0500

    Time: 33 ms (5.28%), Space: 45.2 MB (6.24%) - LeetHub

---
+++ 1058-lexicographically-smallest-equivalent-string/1058-lexicographically-smallest-equivalent-string.java
@@ -0,0 +1,41 @@
+class Solution {
+    public String smallestEquivalentString(String s1, String s2, String baseStr) {
+        Map<Character, List<Character>> mappings = new HashMap<>();
+
+        int strLen = s1.length();
+        
+        for(int i=0; i<strLen; i++){
+            char u = s1.charAt(i);
+            char v = s2.charAt(i);
+            mappings.computeIfAbsent(u, k->new ArrayList<>()).add(v);
+            mappings.computeIfAbsent(v, k->new ArrayList<>()).add(u);
+        }
+
+        boolean[] visited; 
+        StringBuilder result = new StringBuilder();
+
+        for(int i=0; i<baseStr.length(); i++){
+            visited = new boolean[26];
+            char ch = baseStr.charAt(i);
+            char res = dfs(ch, mappings, visited);
+            result.append(res);
+        }
+        return result.toString();
+    }
+
+    public char dfs(char ch, Map<Character, List<Character>> mappings, boolean[] visited){
+
+        visited[ch - 'a'] = true;
+        char minChar = ch;
+        for(char n : mappings.getOrDefault(ch, new ArrayList<>())){
+            
+            if(!visited[n - 'a']){
+                char cnd = dfs(n, mappings, visited);
+                if(cnd < minChar){
+                    minChar = cnd;
+                }
+            }
+        }
+        return minChar;
+    }
+}
