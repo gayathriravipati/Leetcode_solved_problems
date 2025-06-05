db81c2867ef189439282eb34d63b264b1d5d73f9
Gayathri <gayathrir1104@gmail.com>
2025-06-04 19:01:59 -0500

    Time: 6 ms (99.89%), Space: 61 MB (45.18%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.java
+++ 0739-daily-temperatures/0739-daily-temperatures.java
@@ -1,32 +1,22 @@
-class Pair<U, V> {
-    public final U first;
-    public final V second;
-
-    public Pair(U first, V second) {
-        this.first = first;
-        this.second = second;
-    }
-}
-
 class Solution {
     public int[] dailyTemperatures(int[] temperatures) {
-        int length = temperatures.length;
-        int[] res = new int[length];
+        int hottestTemp = 0;
+        int n = temperatures.length;
+        int[] res = new int[n];
 
-        Stack<Pair<Integer, Integer>> st = new Stack<>();
-
-        st.push(new Pair<>(0, temperatures[0]));
-
-        for(int i=1; i<length; i++){
-            while(!st.isEmpty() && st.peek().second < temperatures[i]){
-                Pair<Integer, Integer> current = st.pop();
-                int idx = current.first;
-                res[idx] = i - idx;
+        for(int i = n-1; i>=0; i--){
+            int currentTemp = temperatures[i];
+            if(currentTemp >= hottestTemp){
+                hottestTemp = currentTemp;
+                continue;
             }
-            st.push(new Pair<>(i, temperatures[i]));
-        }
 
-        
+            int days = 1;
+            while(temperatures[i + days] <= currentTemp){
+                days += res[i + days];
+            }
+            res[i] = days;
+        }
         return res;
     }
 }
