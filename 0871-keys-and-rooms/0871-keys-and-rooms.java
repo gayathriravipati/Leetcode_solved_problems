81991c8d3c9ffba90440296195a6cedc654233cf
Gayathri <gayathrir1104@gmail.com>
2025-07-01 18:48:11 -0500

    Time: 2 ms (52.42%), Space: 44.7 MB (24.46%) - LeetHub

--- 0871-keys-and-rooms/0871-keys-and-rooms.java
+++ 0871-keys-and-rooms/0871-keys-and-rooms.java
@@ -3,38 +3,25 @@ class Solution {
         int n = rooms.size();
         boolean[] status = new boolean[n];
         status[0] = true;
-        int visited = 0;
+        int visited = 1;
         Stack<Integer> keys = new Stack<>();
-        List<Integer> currentKeys = rooms.get(0);
-        for(int i : currentKeys){
-            if(!status[i]){
-                keys.add(i);
-                status[i] = true;
-                visited++;
-                if(visited == n-1){
-                    return true;
-                }
-            }
-        }
+        keys.push(0);  
+
 
         while(!keys.isEmpty()){
             int idx = keys.pop();
-            currentKeys = rooms.get(idx);
+            List<Integer> currentKeys = rooms.get(idx);
             for(int i : currentKeys){
                 if(!status[i]){
                     keys.add(i);
                     status[i] = true;
                     visited++;
-                    if(visited == n-1){
+                    if(visited == n){
                         return true;
                     }
                 }
             }
         }
-
-        // for(int i=0; i<n; i++){
-        //     if(!status[i]) return false;
-        // }
 
         return false;
     }
