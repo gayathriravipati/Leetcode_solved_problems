91fa24bbaa41f98c119ee205dc5803ca05135f01
Gayathri <gayathrir1104@gmail.com>
2025-07-01 20:21:27 -0500

    Time: 4 ms (18%), Space: 47.5 MB (72.03%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.java
+++ 0547-number-of-provinces/0547-number-of-provinces.java
@@ -1,55 +1,45 @@
 class Solution {
-    
     public int findCircleNum(int[][] isConnected) {
+       
         int n = isConnected.length;
-         List<Integer>[] adjacencylist = new ArrayList[n];
+        List<List<Integer>> adjacency = new ArrayList<>();
         
-        for(int i = 0; i < n; i++){
-            adjacencylist[i] = new ArrayList<>();
+
+        for(int i = 0; i<n; i++){
+            adjacency.add(new ArrayList<>());
         }
-        
+
+        //Now traverse the matrix to build the list
         for(int i=0; i<isConnected.length; i++){
             for(int j=0; j<isConnected[0].length; j++){
                 if(isConnected[i][j] == 1){
-                    adjacencylist[i].add(j);
+                    adjacency.get(i).add(j);
+                    adjacency.get(j).add(i);
                 }
             }
         }
-        
+
+        //Now that we have adjacency list lets travel and build components.
         boolean[] visited = new boolean[n];
-        int totalProvinces = 0;
-        
-        for(int i=0; i<n; i++){
-            if(!visited[i] && adjacencylist[i].size() > 0){
-                System.out.println(i);
-                totalProvinces++;
-                explore(i, adjacencylist, visited);
+        int component = 0;
+        for(int i = 0; i<n; i++){
+            if(!visited[i]){
+                component++;
+                dfs(visited, i, adjacency);
             }
         }
-        return totalProvinces;
+        return component;
     }
-    
-    private void explore(int idx, List<Integer>[] adjacencylist, boolean[] visited ){
+
+    public void dfs(boolean[] visited, int idx, List<List<Integer>> adjacency){
         visited[idx] = true;
-//         Queue<Integer> q = new LinkedList<>();
-//         q.add(idx);
-        
-//         while(!q.isEmpty()){
-//             int cr = q.poll();
-//             List<Integer> neighbor = adjacencylist[cr];
-//             for(int i=0; i<neighbor.size(); i++){
-//                 if(!visited[neighbor.get(i)]){
-//                     visited[neighbor.get(i)] = true;
-//                     q.add(neighbor.get(i));
-//                 }
-//             }
-//         }
-        List<Integer> neighbor = adjacencylist[idx];
-        for(int n : neighbor){
-            if(!visited[n]){
-                explore(n, adjacencylist, visited);
+
+        List<Integer> cities = adjacency.get(idx);
+
+        for(int city : cities){
+            if(!visited[city]){
+                dfs(visited, city, adjacency);
             }
         }
     }
-    
 }
