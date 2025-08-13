543c64bf85187aeba0f544d87f388c1fac8abe34
Gayathri <gayathrir1104@gmail.com>
2025-08-13 10:23:49 -0500

    Time: 0 ms (100%), Space: 40.8 MB (25.57%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,43 +1,42 @@
-class Solution {
-    public int uniquePaths(int m, int n) {
-        int dp[][] = new int[m][n];
-
-        for(int i = 0; i < m; i++){
-            dp[i][n-1] = 1;
-        }
-
-        for(int i = 0; i < n; i++){
-            dp[m-1][i] = 1;
-        }
-
-        for (int i = m - 2; i >= 0; i--) {
-            for (int j = n - 2; j >= 0; j--) {
-                dp[i][j] = dp[i + 1][j] + dp[i][j + 1];
-            }
-        }
-        return dp[0][0];
-    }
-}
-
-
-// space-optimised version
-
 // class Solution {
 //     public int uniquePaths(int m, int n) {
-//         int[] dp = new int[n];
+//         int[][] dp = new int[m][n];
 
-//         // Last row has only 1 way to move (all right moves)
-//         for (int i = 0; i < n; i++) {
-//             dp[i] = 1;
+//         //setting the last row to 1 => only moving right is the possible option to reach the target node.
+//         for(int i=0; i<n; i++){
+//             dp[m-1][i] = 1;
 //         }
 
-//         // Fill from bottom to top
-//         for (int row = m - 2; row >= 0; row--) {
-//             for (int col = n - 2; col >= 0; col--) {
-//                 dp[col] = dp[col] + dp[col + 1];
+//         //for the last column moving down is the only possibility
+//         for(int j=0; j<m; j++){
+//             dp[j][n-1] = 1;
+//         }
+
+//         for(int i = m-2; i>=0; i--){
+//             for(int j = n-2; j>=0; j--){
+//                 dp[i][j] = dp[i+1][j] + dp[i][j+1];
 //             }
 //         }
 
-//         return dp[0];
+//         return dp[0][0];
 //     }
 // }
+
+
+class Solution {
+    public int uniquePaths(int m, int n) {
+        int[] dp = new int[n];
+
+        for(int i=0; i<n; i++){
+            dp[i] = 1;
+        }
+
+        for(int j = m-2; j>=0; j--){
+            for(int i=n-2; i>=0; i--){
+                dp[i] = dp[i] + dp[i+1];
+            }
+        }
+        
+        return dp[0];
+    }
+}
