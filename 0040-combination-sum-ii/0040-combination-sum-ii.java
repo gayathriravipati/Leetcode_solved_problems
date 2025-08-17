8cb175e064653e2615706ecc2f01469cc79bbf35
Gayathri <gayathrir1104@gmail.com>
2025-08-17 18:37:55 -0500

    Time: 2 ms (99.85%), Space: 43.8 MB (8.6%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.java
+++ 0040-combination-sum-ii/0040-combination-sum-ii.java
@@ -1,33 +1,45 @@
 class Solution {
-    List<List<Integer>> res = new ArrayList<>();
-    int[] candidates;
-    int target;
+     List<List<Integer>> possiblePaths = new ArrayList<>();
+     int[] candidates;
+     int target;
+
     public List<List<Integer>> combinationSum2(int[] candidates, int target) {
+        Arrays.sort(candidates);
         this.candidates = candidates;
         this.target = target;
-        Arrays.sort(candidates);
-        List<Integer> temp = new ArrayList<>();
-        backTrack(0, 0, temp);
-        return res;
+        backTrack(0, 0, new ArrayList<>());
+        return possiblePaths;
     }
 
-    public void backTrack(int idx, int sum, List<Integer> temp){
-        if(sum == target){
-            res.add(new ArrayList<>(temp));
-            return;
-        }
-
+    public void backTrack(int idx, int sum, List<Integer> current){
         if(sum > target){
             return;
         }
 
-        for(int i = idx; i<candidates.length; i++){
-            if (i > idx && candidates[i] == candidates[i - 1]) continue;
-            int newSum = sum + candidates[i];
-            if (newSum > target) break; 
-            temp.add(candidates[i]);
-            backTrack(i+1, sum + candidates[i], temp);
-            temp.remove(temp.size() - 1);
+        if(sum == target){
+            // System.out.println(sum);
+            // System.out.println(current);
+            possiblePaths.add(new ArrayList<>(current));
+            return;
+        }
+
+        for(int i = idx; i< candidates.length; i++){
+
+            if (i > idx && candidates[i] == candidates[i-1]) {
+                continue; 
+            }
+
+            if (sum + candidates[i] > target) {
+                break; 
+            }
+
+            
+
+            current.add(candidates[i]);
+            // System.out.println(current + "list");
+            backTrack(i+1, sum + candidates[i], current);
+            current.remove(current.size() - 1);
+            // System.out.println(current + "last elem removal");
         }
     }
 }
