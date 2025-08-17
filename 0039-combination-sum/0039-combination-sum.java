4ad1475a545faa049d8f3af909679e453937bf27
Gayathri <gayathrir1104@gmail.com>
2025-08-17 16:44:41 -0500

    Time: 2 ms (83.17%), Space: 44.7 MB (45.38%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.java
+++ 0039-combination-sum/0039-combination-sum.java
@@ -1,28 +1,39 @@
 class Solution {
-    List<List<Integer>> res = new ArrayList<>();
+    List<List<Integer>> possiblePaths = new ArrayList<>();
     int[] candidates;
     int target;
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        // sort
+        // idx - tracking => if idx beyond or sum beyond return;
+        // remove last elemet and keep trying combinations with other elements.
+        Arrays.sort(candidates);
         this.candidates = candidates;
         this.target = target;
         backTrack(0, 0, new ArrayList<>());
-        return res;
+        return possiblePaths;
     }
 
-    private void backTrack(int start, int sum, List<Integer> temp){
-        if(sum > target){
+    public void backTrack(int idx, int sum, List<Integer> current){
+        if(idx > candidates.length - 1 || sum > target){
             return;
         }
 
         if(sum == target){
-            res.add(new ArrayList<>(temp));
+            // System.out.println(sum);
+            // System.out.println(current);
+            possiblePaths.add(new ArrayList<>(current));
             return;
         }
 
-        for (int i = start; i < candidates.length; i++) {
-            temp.add(candidates[i]);                      
-            backTrack(i, sum + candidates[i], temp);      
-            temp.remove(temp.size() - 1);                
+        for(int i = idx; i<candidates.length; i++){
+            if (sum + candidates[i] > target) {
+                break; 
+            }
+            current.add(candidates[i]);
+            // System.out.println(current + "list");
+            backTrack(i, sum + candidates[i], current);
+            current.remove(current.size() - 1);
+            // System.out.println(current + "last elem removal");
         }
     }
 }
