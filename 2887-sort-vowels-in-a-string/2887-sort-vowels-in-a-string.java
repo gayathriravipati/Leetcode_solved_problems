4aef954c4840b74225f20f829c41abb0cee7c145
Gayathri <gayathrir1104@gmail.com>
2025-09-12 16:52:46 -0500

    Time: 20 ms (87.63%), Space: 46.2 MB (67.75%) - LeetHub

--- 2887-sort-vowels-in-a-string/2887-sort-vowels-in-a-string.java
+++ 2887-sort-vowels-in-a-string/2887-sort-vowels-in-a-string.java
@@ -1,36 +1,39 @@
-import java.util.*;
-
 class Solution {
+
+    private boolean isVowel(Character ch){
+        if(ch == 'A' || ch == 'E' || ch == 'I'|| ch == 'O'|| ch == 'U'|| 
+        ch == 'a'|| ch == 'e'|| ch == 'i'|| ch == 'o'|| ch == 'u'){
+            return true;
+        }
+        return false;
+    }
+
     public String sortVowels(String s) {
-        // 1. Collect vowels
-        // 2. Sort them
-        // 3. Replace in original order
+        int[] charMap = new int[1000];
 
-        StringBuilder vowelString = new StringBuilder();
-
-        for (char ch : s.toCharArray()) {
-            if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' ||
-                ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U') {
-                vowelString.append(ch);
+        for(char ch : s.toCharArray()){
+            if(isVowel(ch)){
+                charMap[ch - 'A']++;
             }
         }
 
-        char[] charArray = vowelString.toString().toCharArray();
-        Arrays.sort(charArray);
+        String vowelList = "AEIOUaeiou";
+        int j = 0;
 
-        int i = 0;
         StringBuilder res = new StringBuilder();
 
-        for (char ch : s.toCharArray()) {
-            if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' ||
-                ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U') {
-                res.append(charArray[i]);
-                i++;
-            } else {
+        for(char ch : s.toCharArray()){
+            if(isVowel(ch)){
+                while(charMap[vowelList.charAt(j) - 'A'] == 0){
+                    j++;
+                }
+                res.append(vowelList.charAt(j));
+                charMap[vowelList.charAt(j) - 'A']--;
+            }
+            else{
                 res.append(ch);
             }
         }
-
-        return res.toString();
+            return res.toString();
     }
 }
