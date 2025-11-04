42dd00d5e6476c76acd15ad41d652d91943f99ab
Gayathri <gayathrir1104@gmail.com>
2025-11-03 21:27:15 -0600

    Time: 2 ms (84.2%), Space: 47.6 MB (6.81%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -1,20 +1,13 @@
 class Solution {
     public boolean canJump(int[] nums) {
-        //last idx = 0;
-        //target = last_idx
-        //target - i Th target target - i
-        // if(nums.length == 1){
-        //     return true;
-        // }
-        
-       int target = nums.length-1;
-        for(int i = nums.length-2; i>=0; i--){
-            if(nums[i] + i >= target){
-                target = i;
+        int target = nums.length - 1; 
+
+        for (int i = nums.length - 2; i >= 0; i--) {
+            if (i + nums[i] >= target) {
+                target = i; 
             }
         }
-        
-        return target == 0 ? true : false;
-    
+
+        return target == 0;
     }
 }
